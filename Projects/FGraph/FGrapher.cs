3e73db727da9fa0dabd742eec6e455cd6bc991ad
ROBIN\Kurt <KurtA@Gjoll.com>
2020-04-28 13:48:21 -0500

    .

---
+++ Projects/FGraph/FGrapher.cs
@@ -0,0 +1,86 @@
+﻿using Eir.DevTools;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace FGraph
+{
+    class FGrapher : ConverterBase
+    {
+        public string GraphName { get; set; }
+
+        public string OutputDir
+        {
+            get => this.outputDir;
+            set => this.outputDir = Path.GetFullPath(value);
+        }
+        private string outputDir;
+
+        public bool DebugFlag { get; set; } = false;
+
+        void LoadDir(String path)
+        {
+            foreach (String subDir in Directory.GetDirectories(path))
+                LoadDir(subDir);
+            foreach (String file in Directory.GetFiles(path))
+                LoadFile(file);
+        }
+
+        void LoadFile(String path)
+        {
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                dynamic array = JsonConvert.DeserializeObject(json);
+
+                foreach (dynamic item in array)
+                    LoadItem(item);
+            }
+        }
+
+        void LoadItem(dynamic item)
+        {
+            JObject j = item as JObject;
+            foreach (KeyValuePair<String, JToken> kvp in j)
+                LoadItem(kvp.Key, kvp.Value);
+        }
+
+
+        void LoadItem(String type, dynamic value)
+        {
+            if (value.graphName != this.GraphName)
+                return;
+
+            switch (type)
+            {
+                case "graphNode":
+                    break;
+                case "graphLinkByName":
+                    break;
+                default:
+                    this.ConversionError("FGrapher",
+                        "Load",
+                        $"unknown graph item '{type}'");
+                    return;
+            }
+        }
+
+        public void Load(String path)
+        {
+            path = Path.GetFullPath(path);
+            if (Directory.Exists(path) == true)
+                LoadDir(path);
+            else if (File.Exists(path))
+                LoadFile(path);
+            else
+            {
+                this.ConversionError("FGrapher",
+                    "Load",
+                    $"{path} not found");
+            }
+        }
+    }
+}
---
+++ Projects/FGraph/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+
+namespace FGraph
+{
+    class Program
+    {
+        FGrapher fGrapher;
+
+        public Program()
+        {
+            this.fGrapher = new FGrapher();
+            this.fGrapher.OutputDir = Path.GetFullPath(".\\Graphs");
+            this.fGrapher.ConsoleLogging();
+        }
+
+        void ParseArguments(String[] args)
+        {
+            Int32 i = 0;
+
+            String GetArg(String errorMsg)
+            {
+                if (i >= args.Length)
+                    throw new Exception($"Missing {errorMsg} parameter");
+
+                String arg = args[i++].Trim();
+                if (arg.Length > 0)
+                {
+                    if ((arg[0] == '"') && (arg[arg.Length - 1] == '"'))
+                        arg = arg.Substring(1, arg.Length - 2);
+                }
+
+                return arg;
+            }
+
+            while (i < args.Length)
+            {
+                String arg = GetArg("arg").ToUpper();
+                switch (arg.ToLower())
+                {
+                    case "-g":
+                        String graphName= GetArg("-g");
+                        this.fGrapher.GraphName = graphName;
+                        break;
+
+                    case "-i":
+                        String pathArg = GetArg("-i");
+                        this.fGrapher.Load(pathArg);
+                        break;
+
+                    case "-o":
+                        this.fGrapher.OutputDir = GetArg("-o");
+                        break;
+
+                    default:
+                        throw new Exception($"Unknown arg {arg}");
+                }
+            }
+        }
+
+        bool Process()
+        {
+            //this.fGrapher.Process();
+            //this.fGrapher.SaveAll();
+            return this.fGrapher.HasErrors == false;
+        }
+
+        static Int32 Main(string[] args)
+        {
+            try
+            {
+                Program p = new Program();
+                p.ParseArguments(args);
+                if (p.Process() == false)
+                    return -1;
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return -1;
+            }
+        }
+    }
+}
